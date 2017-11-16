030f63f725788428428b22df07dd9be13655db46
Minwoo Kim <minwookim@Minwoos-MacBook-Pro.local>
2017-11-16 13:20:10 -0600

    change env.port

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const path = require('path')
 const db = require('./db/config');
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), () => {
-  console.log(`Server running, listening on ${app.get('port')}...`);	
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
